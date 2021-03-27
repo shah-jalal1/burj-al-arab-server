15a641d3179f31e3793d7c5abe600618d6b5db4e
shahjalalcse <shah-jalal1>
2021-03-27 22:15:16 +0600

    create done

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const boyParser = require('body-parser');
+const cors = require('cors');
+
+
+const port = 5000
+
+app.use(cors());
+app.use(boyParser.json());
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://jalal406:jalal2009@cluster0.owenr.mongodb.net/burjAlArab?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const bookings = client.db("burjAlArab").collection("bookings");
+    console.log("db connected successfully");
+    
+    app.post('/addBooking', (req, res) => {
+        const newBooking = req.body;
+        bookings.insertOne(newBooking)
+        .then(result => {
+            // console.log(result);
+            res.send(result.insertedCount > 0)
+
+        })
+        // console.log(newBooking);
+    })
+
+
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port)
